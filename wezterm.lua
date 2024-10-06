939497b0b47be204d9f4fb0578f83b3f2934f571
Tyler Kramlich <kramlichtyler@gmail.com>
2024-10-06 16:43:49 -0400

    update color

--- wezterm.lua
+++ wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Builtin Solarized Dark'
+config.color_scheme = 'AdventureTime'
 config.window_close_confirmation = 'NeverPrompt'
 
 return config
